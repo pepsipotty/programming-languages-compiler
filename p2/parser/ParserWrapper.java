eafe6e8dc8742bc5215adf3f2c23c143ce0b6253
pepsipotty <smkkhanz@gmail.com>
2024-01-27 02:20:06 -0500

    Add new files and make changes to existing files

---
+++ p2/parser/ParserWrapper.java
@@ -0,0 +1,14 @@
+package parser;
+import java.io.Reader;
+import java_cup.runtime.*;
+import interpreter.Interpreter;
+import ast.Program;
+
+public class ParserWrapper {
+    public static Program parse(Reader reader) throws Exception {
+        ComplexSymbolFactory csf = new ComplexSymbolFactory();
+        Lexer scanner = new Lexer(reader, csf);
+        Parser parser = new Parser(scanner, csf);
+	return (Program)parser.parse().value;
+    }
+}
