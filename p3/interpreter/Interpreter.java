d12b8db3ad8c1ebbc62bc3ed47c1c76632cb93c1
yeoh.29 <yeoh.29@cse-sl3.coeit.osu.edu>
2024-02-20 22:06:12 -0500

    p3

---
+++ p3/interpreter/Interpreter.java
@@ -0,0 +1,38 @@
+package interpreter;
+
+import java.io.*;
+import java.util.*;
+import parser.ParserWrapper;
+import ast.Program;
+
+public class Interpreter {
+
+    // Process return codes
+    public static final int EXIT_SUCCESS = 0;
+    public static final int EXIT_PARSING_ERROR = 1;
+    public static final int EXIT_STATIC_CHECKING_ERROR = 2;
+    public static final int EXIT_UNINITIALIZED_VAR_ERROR = 3;
+    public static final int EXIT_DIV_BY_ZERO_ERROR = 4;
+    public static final int EXIT_FAILED_STDIN_READ = 5;
+    public static void main(String[] args) {
+        String filename = args[0];
+        Program astRoot = null;
+        BufferedReader reader;
+        try {
+            reader = new BufferedReader(new FileReader(filename));
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+        try {
+	    astRoot = ParserWrapper.parse(reader);
+        } catch (Exception ex) {
+            Interpreter.fatalError("Uncaught parsing error: " + ex, Interpreter.EXIT_PARSING_ERROR);
+        }
+	// for debugging
+	astRoot.print(System.out);
+    }
+    public static void fatalError(String message, int processReturnCode) {
+        System.out.println(message);
+        System.exit(processReturnCode);
+    }
+}
