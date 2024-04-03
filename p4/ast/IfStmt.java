b5f95736667549e894f83c0eb1de72a049d79419
pepsipotty <smkkhanz@gmail.com>
2024-04-03 02:23:26 -0400

    Reduce spaghetti logic and nonsense baloney logic. Not in my original submission. -1.5pts

--- p4/ast/IfStmt.java
+++ p4/ast/IfStmt.java
@@ -32,28 +32,23 @@ public class IfStmt extends Stmt {
     }
 
 	public void execute() {
-		if (elsestmt != null) {
-			table.newState(); // create s1
-			thenstmt.execute(); //execute the then block on state s. We don't need to create a temporary state. We modify the OG state directly
-			HashMap<String, AbstractValue> s1 = table.getTempState(); //get the state s1
-			table.abortTempState(); //abort the temporary state s1, we've made a copy of it and don't need it anymore
-	
-			table.newState(); //create a new state, s2 for the else statement to operate on
-			elsestmt.execute(); // everything here is operating on the new state, s2, and not the original state s1. 
-			HashMap<String, AbstractValue> s2 = table.getTempState(); //get the state s2
-			table.abortTempState();
-	
-			table.mergeState(s1, s2); //merge the two states together, s2 and s to get the final state. After merge, s2 is deleted and we revert back to the OG state s
-		} else {
-			HashMap<String, AbstractValue> s1 = new HashMap<String, AbstractValue>(table.getOriginalState());
+		if (elsestmt == null) {
+			HashMap<String, AbstractValue> s0 = new HashMap<>(table.getOriginalState());
 			thenstmt.execute();
-			HashMap<String, AbstractValue> s2 = new HashMap<String, AbstractValue>(table.getOriginalState());
-			table.mergeState(s1, s2);
-
+			HashMap<String, AbstractValue> s1 = new HashMap<>(table.getOriginalState());
+			HashMap<String, AbstractValue> mergedState =  table.mergeStateWhile(s0, s1);
+			table.setOriginalState(mergedState);
+		} else {
+			HashMap<String, AbstractValue> s0 = new HashMap<>(table.getOriginalState());
+			thenstmt.execute();
+			HashMap<String, AbstractValue> s1 = new HashMap<>(table.getOriginalState());
+			
+			table.setOriginalState(s0);
+			elsestmt.execute();
+			HashMap<String, AbstractValue> s2 = new HashMap<>(table.getOriginalState());
+			HashMap<String, AbstractValue> mergedState =  table.mergeStateWhile(s1, s2);
+			table.setOriginalState(mergedState);
 		}
-
-
-
 
     }
 }
